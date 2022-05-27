9839becccd564d7e3c7e92b95d46526fcd239320
Mehrnaz <mehrnazhemmat1998@gmail.com>
2022-05-27 21:51:04 +0430

    add some styles on trash icon and border it :approach 1

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,22 +1,25 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import style from "./product.module.css";
 import { BiTrash } from "react-icons/bi";
 
 export default function Product(props) {
-  const [userName,setUsername] = useState("")
+  const [userName, setUsername] = useState("");
 
-  const changeHandler =(e) => {
-    setUsername(e.target.value)
-  }
+  const changeHandler = (e) => {
+    setUsername(e.target.value);
+  };
 
   return (
     <div className={style.product} onClick={props.product.click}>
-      <input className={style.input} type="text" value={userName} onChange={changeHandler}/>
-      <p>
-       { `Product name: ${props.product.title}`}
-      </p>
+      <input
+        className={style.input}
+        type="text"
+        value={userName}
+        onChange={changeHandler}
+      />
+      <p>{`Product name: ${props.product.title}`}</p>
       <p>{`Product price: ${props.product.price}`}</p>
-     
+
       {props.children}
       <span className={style.value}>{props.product.quantity}</span>
       <input
@@ -25,13 +28,18 @@ export default function Product(props) {
         onChange={props.onChange}
         value={props.product.title}
       />
-        
-      <button onClick={props.onDecrement} className={`${style.button} ${style.inc}`}>
-       {props.product.quantity > 1 ? "-" : <BiTrash/>}
+
+      <button
+        onClick={props.onDecrement}
+        className={`${style.button} ${props.product.quantity ==1 ? style.remove : null}`}
+      >
+      
+        {props.product.quantity > 1 ? "-" : <BiTrash />}
       </button>
+
       <button
         onClick={props.onIncrement}
-        className={`${style.button} ${style.inc}`}
+        className={`${style.button} `}
       >
         +
       </button>
