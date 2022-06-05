8d64955a72761e70fb3f801bdba8539d8de4f52e
Mehrnaz <mehrnazhemmat1998@gmail.com>
2022-06-06 02:06:51 +0430

    2-46-higher order component(hoc)

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@
 import React, { Component } from "react";
 import ProductList from "./Components/ProductList/ProductList";
 import Navbar from "./Components/Navbar/Navbar";
-import ClassCounter from './Components/ClassCounter';
-import FunctionalCounter from './Components/FunctionalCounter';
+import ClassCounter from "./Components/ClassCounter";
+import FunctionalCounter from "./Components/FunctionalCounter";
 import ClassTimer from "./Components/ClassTimer";
-import style from './index.css'
-import FunctionTimer from './Components/FunctionTimer';
+import style from "./index.css";
+import FunctionTimer from "./Components/FunctionTimer";
+import wrapper from "./Components/hoc/wrapper";
 export class App extends Component {
-  constructor (props) {
-    super (props) 
-    console.log("App.js constructor")
+  constructor(props) {
+    super(props);
+    console.log("App.js constructor");
   }
   state = {
     products: [
@@ -18,7 +19,7 @@ export class App extends Component {
       { title: "NodeJs", price: "70$", id: 2, quantity: 5 },
       { title: "React.js", price: "80$", id: 3, quantity: 9 },
     ],
-    isShow:true,
+    isShow: true,
   };
 
   removeHandler = (id) => {
@@ -31,70 +32,65 @@ export class App extends Component {
   incrementHandler = (id) => {
     //1. id ==> ok
     //2. index
-    const index = this.state.products.findIndex((item) => item.id === id)
-    console.log(index)
+    const index = this.state.products.findIndex((item) => item.id === id);
+    console.log(index);
     //3.Clone the selected index and update the quantity:
-    const product = {...this.state.products[index]};
+    const product = { ...this.state.products[index] };
     product.quantity++;
     //4. update products:
     const products = [...this.state.products];
     products[index] = product;
-    this.setState({products})
+    this.setState({ products });
   };
 
   changeHandlere = (event, id) => {
-    const index = this.state.products.findIndex(item => item.id === id); //1. found index
-    const product = {...this.state.products[index]}  //2. we found product //obj
+    const index = this.state.products.findIndex((item) => item.id === id); //1. found index
+    const product = { ...this.state.products[index] }; //2. we found product //obj
     product.title = event.target.value;
-    const products = [...this.state.products]
-    products[index] = product
+    const products = [...this.state.products];
+    products[index] = product;
     this.setState({ products: products });
   };
 
   decrementHandler = (id) => {
-    const index = this.state.products.findIndex(item => item.id === id);
-    const product = {...this.state.products[index]} 
+    const index = this.state.products.findIndex((item) => item.id === id);
+    const product = { ...this.state.products[index] };
     product.quantity--;
     const products = [...this.state.products];
     products[index] = product;
     if (product.quantity <= 0) {
-      const filteredProduct = this.state.products.filter(p => p.id !== id);
-      this.setState({products:filteredProduct})
+      const filteredProduct = this.state.products.filter((p) => p.id !== id);
+      this.setState({ products: filteredProduct });
     } else {
-      this.setState({products})
-    }    
+      this.setState({ products });
+    }
   };
 
-  componentDidMount() { 
-    console.log("App.js componentDidMount() ")
-    //Ajax : 
+  componentDidMount() {
+    console.log("App.js componentDidMount() ");
+    //Ajax :
     // Products => this.setState({Products...})
-   }
-   componentDidUpdate(prevProps,prevState) {
-    console.log("App.js componentDidUpdate()")
-    console.log("App.js",prevState)
-   }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log("App.js componentDidUpdate()");
+    console.log("App.js", prevState);
+  }
   render() {
-    console.log("App.js render()")
+    console.log("App.js render()");
     return (
-     
-      <div className="container" id="title">
-        {/* <Navbar totalItems={this.state.products.filter((p) => p.quantity > 0).length} /> */}
-        {/* <ProductList
+      <wrapper class="container">
+        <Navbar
+          totalItems={this.state.products.filter((p) => p.quantity > 0).length}
+        />
+
+        <ProductList
           products={this.state.products}
           onRemove={this.removeHandler}
           onIncrement={this.incrementHandler}
           onChange={this.changeHandlere}
           onDecrement={this.decrementHandler}
-        /> */}
-        {/* <FunctionalCounter/> */}
-        <button className={this.state.isShow ? "bgcred" : "bgcgreen"} style={{width:"55px",height:"25px"}} onClick={()=> this.setState({isShow:!this.state.isShow})}>{this.state.isShow ? "hide" : "show"}</button>
-         {/* {this.state.isShow ? <ClassTimer/> : null}  //solution 1 */}
-         {/* {console.log(this.state.isShow)} */}
-         {/* {this.state.isShow && <ClassTimer/> } */}
-         {/* <FunctionTimer/> */}
-
-      </div>
+        />
+      </wrapper>
     );
   }
 }
